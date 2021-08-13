e9c21bd1aaddb579b854a98dddca4bd32fa4c8af
Dusko Sretenovic <dsretenovic555@gmail.com>
2021-08-13 14:00:08 +0200

    Initial commit

---
+++ SS_domaci_windows/assembler.hpp
@@ -0,0 +1,9 @@
+#ifndef ASSEMBLER_H
+#define ASSEMBLER_H
+
+class Assembler {
+
+
+};
+
+#endif
---
+++ SS_domaci_windows/main.cpp
@@ -0,0 +1,11 @@
+#include "assembler.hpp"
+
+int main(int argc, char** argv) {
+
+
+    Assembler* assembler = new Assembler();
+
+    delete assembler;
+
+    return 0;
+}
